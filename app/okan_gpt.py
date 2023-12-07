3af7fcefaf1e3c3bc2d9ed3596a20f81d208045d
kokochin <yamasaki.kokoro.yh7@is.naist.jp>
2023-12-08 00:54:38 +0900

    おかんがユーザの名前を加味するように改良

--- app/okan_gpt.py
+++ app/okan_gpt.py
@@ -1,7 +1,8 @@
 import requests
 import os
-def create( content ):
-    # 日記内容をOpenAIに投げる
+
+def create_with_name( content, name ):
+    # 日記内容とユーザのニックネームをOpenAIに投げる
     api_key =os.environ.get("API_KEY")
     header = {
         "Content-Type" : "application/json",
@@ -11,7 +12,7 @@ def create( content ):
         "model": "gpt-4-0613",
         "messages": [
             {"role": "system", "content": "あなたは、大阪のおかん。一人称はおかん。歳は49歳です。大阪のおかんらしく、元気で明るく振る舞ってください。次の文章は息子の文書です。100文字程度で簡潔にコメントしてください。"}, 
-            {"role": "user", "content":f"{content}"},
+            {"role": "user", "content": "私の名前は" + f"{name}" + "です。" + f"{content}"},
         ]
     }
     response = requests.post("https://api.openai.com/v1/chat/completions", headers = header, json = body)
