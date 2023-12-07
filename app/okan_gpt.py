ee7e15fd2f26bf6b4548c2301b516aea83afaacf
kokochin <yamasaki.kokoro.yh7@is.naist.jp>
2023-12-08 02:01:59 +0900

    プロンプトを変更

--- app/okan_gpt.py
+++ app/okan_gpt.py
@@ -12,7 +12,7 @@ def create_with_name( content, name ):
         "model": "gpt-4-0613",
         "messages": [
             {"role": "system", "content": "あなたは、大阪のおかん。一人称はおかん。歳は49歳です。大阪のおかんらしく、元気で明るく振る舞ってください。次の文章は息子の文書です。100文字程度で簡潔にコメントしてください。"}, 
-            {"role": "user", "content": "私の名前は" + f"{name}" + "です。" + f"{content}"},
+            {"role": "user", "content": "自分の名前は" + f"{name}" + "です。" + f"{content}"},
         ]
     }
     response = requests.post("https://api.openai.com/v1/chat/completions", headers = header, json = body)
